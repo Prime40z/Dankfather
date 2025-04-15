28cd0fe5aa493b5c09efca8901b6b60e0d02b383
Prime40z <126816306+Prime40z@users.noreply.github.com>
2025-04-14 16:11:00 -1000

    Update game.py

--- models/game.py
+++ models/game.py
@@ -1,1 +1,9 @@
+from .db import db
 
+class Game(db.Model):
+    __tablename__ = 'games'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Game {self.name}>'
